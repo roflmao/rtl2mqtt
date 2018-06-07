8cd15c73997edfdf69e9c5a51335a64df1d82f78
Thomas Wenzlaff <karotz@wenzlaff.de>
2018-06-07 22:42:53 +0200

    Update rtl2mqtt.sh
    Set Topic

--- rtl2mqtt.sh
+++ rtl2mqtt.sh
@@ -5,15 +5,15 @@
 # Author: Marco Verleun <marco@marcoach.nl>
 # Version 2.0: Adapted for the new output format of rtl_433
 
-# Remove hash on next line for debugging
-#set -x
+# Set Host
+MQTT_HOST="test.mosquitto.org"
+# Set Topic
+TOPIC="/Data/Rtl/433"
 
 export LANG=C
 PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin"
 
-#
 # Start the listener and enter an endless loop
-#
 /usr/local/bin/rtl_433 -F json |  while read line
 do
 # Log to file if file exists.
@@ -21,5 +21,5 @@ do
   [ -w /tmp/rtl_433.log ] && echo $line >> rtl_433.log
 
 # Raw message to MQTT
-  echo $line  | /usr/bin/mosquitto_pub -h $MQTT_HOST -i RTL_433 -l -t "RTL_433/Raw"
+  echo $line  | /usr/bin/mosquitto_pub -h $MQTT_HOST -i RTL_433 -l -t $TOPIC
 done
