ffb3bc01b5ea9a2c8334141c6bde30a56cc36d9b
rolf <rolf@supereye.org>
2018-12-27 12:53:04 +0100

    touch yolo

--- rtl2mqtt.sh
+++ rtl2mqtt.sh
@@ -18,7 +18,7 @@ PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin"
 do
 # Log to file if file exists.
 # Create file with touch /tmp/rtl_433.log if logging is needed
-  [ -w /tmp/rtl_433.log ] && echo $line >> rtl_433.log
+  [ -w /rtl_433.log ] && echo $line >> rtl_433.log
 
 # Raw message to MQTT
   echo $line  | /usr/bin/mosquitto_pub -h $MQTT_HOST -i RTL_433 -l -t "RTL_433/Raw"
